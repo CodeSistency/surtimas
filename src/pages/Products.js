9cda8cbaee8f51fc98414c951a2414203668e2f3
Jose Perez <valero.jozee@gmail.com>
2023-08-11 09:48:07 -0400

    config axios normal

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {useContext} from 'react'
 import CartContext from "../context/CartProvider";
 import { axiosPrivate } from '../api/axios';
+import axios from '../api/axios';
 import { useState, useEffect } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { useNavigate, useLocation, Link } from "react-router-dom";
@@ -21,7 +22,7 @@ function Products() {
 
         const getProducts = async () => {
             try {
-                const response = await axiosPrivate.get('/products', {
+                const response = await axios.get('/products', {
                     signal: controller.signal
                 });
                 console.log(response.data);
