bd5e7232ba09413f0284c7e9102b43388335b09a
Jose Perez <valero.jozee@gmail.com>
2023-08-10 21:28:30 -0400

    ddin axios url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:3500';
-// const BASE_URL = 'https://backend-5m1g.onrender.com';
+// const BASE_URL = 'http://localhost:3500';
+const BASE_URL = 'https://backend-5m1g.onrender.com';
 
 export default axios.create({
     baseURL: BASE_URL
