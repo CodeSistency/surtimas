337c1152cdb13ebe656c8a14dbdffa18c6438a92
CodeSistency <121405182+CodeSistency@users.noreply.github.com>
2023-09-28 21:37:31 -0400

    dg

--- src/components/Products.js
+++ src/components/Products.js
@@ -257,7 +257,7 @@ const Products = () => {
                                 <Link  to={`products/${product._id}`} style={{color: "black"}}><MdOutlineModeEditOutline fontSize={27} style={{marginTop:'7px'}}/></Link>
                                 </td>
                                 <td>
-                                {/* <MdDeleteForever fontSize={27} style={{marginTop:'7px', cursor: 'pointer'}} onClick={() => handleDelete(product._id)}/> */}
+                                <MdDeleteForever fontSize={27} style={{marginTop:'7px', cursor: 'pointer'}} onClick={handleDelete(product._id)}/>
                                 </td>
                                 </tr>
                             ))}
@@ -318,8 +318,8 @@ const Products = () => {
                                 <Link  to={`products/${product._id}`} style={{color: "black"}}><MdOutlineModeEditOutline fontSize={27} style={{marginTop:'7px'}}/></Link>
                                 </td>
                                 <td>
-                                {/* <MdDeleteForever fontSize={27} style={{marginTop:'7px', cursor: 'pointer'}} onClick={handleDelete(product._id)}/> */}
-                                {/* <MdDeleteForever fontSize={27} style={{marginTop:'7px', cursor: 'pointer'}} onClick={() => handleDelete(product._id)}/> */}
+                                <MdDeleteForever fontSize={27} style={{marginTop:'7px', cursor: 'pointer'}} onClick={handleDelete(product._id)}/>
+
         
                                 </td>
                                 </tr>
