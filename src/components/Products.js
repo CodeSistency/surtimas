a06bf80847726f0daa5c11fdba0e561b6501bfbe
CodeSistency <121405182+CodeSistency@users.noreply.github.com>
2023-10-15 11:21:50 -0400

    algo

--- src/components/Products.js
+++ src/components/Products.js
@@ -320,7 +320,7 @@ const Products = () => {
                                 <Link  to={`products/${product._id}`} style={{color: "black"}}><MdOutlineModeEditOutline fontSize={27} style={{marginTop:'7px'}}/></Link>
                                 </td>
                                 <td>
-                                {/* <MdDeleteForever fontSize={27} style={{marginTop:'7px', cursor: 'pointer'}} onClick={handleDelete(product._id)}/> */}
+                                <MdDeleteForever fontSize={27} style={{marginTop:'7px', cursor: 'pointer'}} onClick={() => handleDelete(product._id)}/>
 
         
                                 </td>
