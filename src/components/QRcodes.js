c8ecc47459e87418742e1b66515ca6c15e5dbb5c
CodeSistency <121405182+CodeSistency@users.noreply.github.com>
2023-10-15 10:56:46 -0400

    algo

--- src/components/QRcodes.js
+++ src/components/QRcodes.js
@@ -8,6 +8,7 @@ import AdminNav from "./AdminNav";
 import axios from "../api/axios";
 import Webcam from "react-webcam";
 import exportAsImage from "../utils/exportAsImage";
+import { useReactToPrint } from 'react-to-print';
 
 const QRcodes = () => {
     const [products, setProducts] = useState();
@@ -24,13 +25,38 @@ const QRcodes = () => {
     facingMode: "user"
   };
 
+  const pageStyle =`
+  @page {
+      size: 30mm 20mm
+  };
+      
+  @media all {
+      .pageBreak {
+          display: none
+      }
+  };
+  
+  @media print{
+      .pageBreak {
+          page-break-before: always;
+      }
+  
+  }
+  
+  `
+
     const ref = useRef()
     
+    const handlePrint = useReactToPrint({
+      content: () => ref.current,
+      pageStyle: pageStyle
+    });
 
     const getProducts = async (pageNumber) => {
         try {
             // const response = await axios.get(`/limited?pageNumber=${pageNumber}&search=${searchQuery}`);
-            const response = await axios.get(`productos/qr?pageNumber=${pageNumber}`);
+            // const response = await axios.get(`productos/qr?pageNumber=${pageNumber}`);
+            const response = await axios.get(`productos`);
             // console.log(props.mujer)
             console.log(response.data);
             setProducts(response.data);
@@ -103,33 +129,15 @@ const QRcodes = () => {
 
     const exportRef = useRef();
 
-    const pageStyle =`
-@page {
-    size: 30mm 20mm
-};
-    
-@media all {
-    .pageBreak {
-        display: none
-    }
-};
 
-@media print{
-    .pageBreak {
-        page-break-before: always;
-    }
-
-}
-
-`
 
     return (
         
-        <article className='dashboard admin-container' ref={ref}>
+        <article className='dashboard admin-container' >
             <AdminNav />
             {products?.length
                 ? (
-                   <div className='qr'>
+                   <div className='qr' ref={ref}>
                     <ul className="qr-list">
                         
                     {products.map((product, i) =>
@@ -138,11 +146,7 @@ const QRcodes = () => {
     color.quantity > 0 && 
     <li>
            <QRcode id={product?.codigo} />
-           <p style={{ marginTop: '-5px', fontSize: '8px', maxWidth: '130px'}}>
-           {product.titulo}
-             {size}
-             {color.color}
-             {product.codigo}
+           <p style={{ marginTop: '-5px', fontSize: '8px', maxWidth: '130px'}}>{`${product.titulo}-${size}-${color.color_seleccionado}-${product.codigo}`}
            </p>              
          </li>
       
@@ -163,7 +167,7 @@ const QRcodes = () => {
   )
 )}
                         </ul>
-                        <div className='pagination'>
+                        {/* <div className='pagination'>
                             {Array.from({ length: totalPages }, (_, i) => i + 1).map((page) => (
                             <button
                                 className={`page ${page === currentPage && 'page-selected'}`}
@@ -174,16 +178,17 @@ const QRcodes = () => {
                                 {page}
                             </button>
                             ))}
-                        </div>
+                        </div> */}
                         {/* <button onClick={() => exportAsImage(exportRef.current, "test")}>
 Capture Image
 </button> */}
-                    <ReactToPrint
+                    {/* <ReactToPrint
                         trigger={() => <button>Print</button>}
                         content={() => ref.current}
                         pageStyle={pageStyle}
                         
-                    /> 
+                    />  */}
+                    <button onClick={handlePrint}>Print</button>
                     {/* <Webcam
     audio={false}
     height={720}
